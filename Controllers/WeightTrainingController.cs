bc7d8c87ae386c4376f6cc8206e0034ea0200293
Meng <damon.m.deng@outlook.com>
2020-04-10 11:27:30 -0700

    Add files via upload

---
+++ Controllers/WeightTrainingController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BodyBuilding.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BodyBuilding.Controllers
+{
+    public class WeightTrainingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("WeightTraining")]
+        [HttpGet]
+        public IActionResult CreateWeightTraining()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateWeightTraining2(WeightTraining wt)
+        {
+            if (!ModelState.IsValid)
+                return View("CreateWeightTraining", wt);
+            else
+                return View("CreateWeightTraining2", wt);
+        }
+    }
+}
